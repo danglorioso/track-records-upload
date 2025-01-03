99e0f1d81d0928dd39c97d3128e0314f6db234ad
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-03 04:52:17 -0500

    Added step 1 to instructions box component

--- src/app/Instructions.tsx
+++ src/app/Instructions.tsx
@@ -1,20 +1,22 @@
 'use client'
 
 import React from 'react'
+import Options from './Options'
 
 const Instructions = () => {
   return (
 
-    <div>
+    <div className="justify-center h-full w-11/12 max-w-4xl">
       <div className="bg-gray-800 rounded-xl overflow-hidden shadow-lg">
         <div className="bg-gray-700 text-white p-6 text-center text-xl font-semibold">
-          <h1>Instructions</h1>
+          <h1>Track Records Upload</h1>
         </div>
-        <div className="p-6">
-          <p className="text-gray-300">
-            Flight information will appear here. Coming soon.
-          </p>
-        </div>
+        {/* Step 1 */}
+        <Options></Options>
+
+        {/* Step 2 */}
+
+        {/* Step 3 */}
       </div>
     </div>
   );
