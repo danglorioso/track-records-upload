97c8bcf31741ccd1ecf5c8a0e995eb84f6add67e
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-03 04:52:38 -0500

    Working page with box with step 1 and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Instructions from "./Instructions";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
-      <Instructions></Instructions>
+    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center">
+      {/* Instructions in the center */}
+      <div className="flex flex-col items-center justify-center flex-1 w-full">
+        <Instructions />
+      </div>
+
+      {/* Footer */}
+      <Footer className="w-full" />
     </div>
   );
 }
