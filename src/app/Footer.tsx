5fe00581b6fd84803acb0a0698be403809c4bcb5
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-03 04:52:24 -0500

    Added footer with link

---
+++ src/app/Footer.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-gray-400 text-center p-4">
+      <p>
+        Developed by{" "}
+        <a
+          href="https://danglorioso.com" // Replace with your actual website URL
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:text-blue-700"
+        >
+          Dan Glorioso
+        </a>
+        . Inspired by Jonathan McGurrin.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer
