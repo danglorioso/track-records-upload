836ba40b9401435f096768acdb301d89c0242d0a
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-02-26 03:16:26 -0500

    Styling to fit entire screen height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Footer from "./Footer";
 
 export default function Home() {
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center pt-8">
+    <div className="bg-gray-900 h-screen flex flex-col items-center pt-8">
       {/* Instructions in the center */}
-      <div className="flex flex-col items-center justify-center flex-1 w-full">
+      <div className="flex flex-col items-center justify-center flex-grow w-full max-w-4-xl px-4 max-h-[calc(100vh-100px)]">
         <Instructions />
       </div>
 
-      {/* Footer */}
+      {/* Footer - always at bottom */}
       <Footer />
     </div>
   );
